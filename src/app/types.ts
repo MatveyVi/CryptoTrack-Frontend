e97e037ae1e4d08b5c996d95e7c8e4e0f2d94cf9
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2025-05-05 19:27:34 +0300

    user and coin types, cleared store

---
+++ src/app/types.ts
@@ -0,0 +1,27 @@
+export type User = {
+    id: string; 
+    name: string;
+    email: string;
+    avatarUrl?: string;
+    bio?: string;
+    location?: string;
+    createdAt: string;
+    updatedAt: string;
+    //posts: Post[];
+    followers: string[];
+    following: string[];
+}
+
+export type Coin = {
+    id: string;
+    symbol: string;
+    name: string;
+    image: string | { [key: string] : string };
+    current_price: number;
+    market_cap: number;
+    market_cap_rank: number;
+    total_volume: number;
+    price_change_percentage_24h: number;
+    createdAt: string;
+    updatedAt: string;
+}
