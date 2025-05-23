6bbb2ff68bc971c9bab7da94653606727ad2e875
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2025-05-23 20:25:35 +0300

    finished current-coin info

--- src/components/coin-info/index.tsx
+++ src/components/coin-info/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import { useCoinByIdQuery } from '../../app/services/coinApi'
 import { useParams } from 'react-router-dom'
-import { Button, Card } from '@heroui/react';
+import { Button, Card, Divider } from '@heroui/react';
 import { FiStar } from 'react-icons/fi';
 import { FaRegStar } from 'react-icons/fa';
 import { IoShareSocialSharp } from 'react-icons/io5';
@@ -12,20 +12,53 @@ type Props = {
 
 export const CoinInfo: React.FC<Props> = ({ id }) => {
   const { data, isLoading } = useCoinByIdQuery(id || '')
-  const getImageUrl = (image: string | { [key: string]: string | undefined}): string | undefined => {
+  const getImageUrl = (image: string | { [key: string]: string | undefined }): string | undefined => {
     if (!image) return undefined
     if (typeof image === 'string') return image
     return image['small'] || Object.values(image)[0]
   }
+
+  const formattedPrice = (price: number): string => {
+    return price.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    })
+  }
+  const formattedPercentage = (percentage: number): JSX.Element => {
+    const formatted = percentage.toFixed(2)
+    return (
+      <span className={percentage >= 0 ? 'text-green-500' : 'text-red-500'}>
+        {percentage >= 0 ? '+' : ''}
+        {formatted}%
+      </span>
+    )
+  }
+  const formatBillions = (num: number) => {
+    return `$${(num / 1_000_000_000).toFixed(2)}B`
+  }
+  const formatSupply = (supply: number): string => {
+    if (supply >= 1_000_000_000) {
+      return (supply / 1_000_000_000).toFixed(2) + 'B'
+    }
+    if (supply >= 1_000_000) {
+      return (supply / 1_000_000).toFixed(2) + 'M'
+    }
+    if (supply >= 1_000) {
+      return supply.toLocaleString('en-US')
+    }
+    return supply.toString()
+  }
+
   return (
-     <div className='flex'>
+    <div className='mx-3'>
+      <div className='flex'>
         <div className='flex flex-row'>
           <img src={getImageUrl(data?.image || '')} alt={data?.name} className='w-8 h-8 rounded-full my-auto mr-2' />
           <p className='text-semibold font-bold mt-1 mr-1'>{data?.name}</p>
           <p className='text-md text-muted-foreground opacity-65 mt-1 mr-1'>{data?.symbol.toUpperCase()}</p>
           <Card className='px-1 mt-1'>#{data?.market_cap_rank}</Card>
         </div>
-        <div className='flex ml-auto mr-4 space-x-2'>
+        <div className='flex ml-auto space-x-2'>
           <Button size='sm'>
             <FaRegStar />
           </Button>
@@ -33,6 +66,42 @@ export const CoinInfo: React.FC<Props> = ({ id }) => {
             <IoShareSocialSharp />
           </Button>
         </div>
-     </div>
+      </div>
+      <Card className='flex mt-8 gap-4 p-4 font-bold'>{typeof data?.current_price === 'number' && (
+        <div className='flex flex-row gap-3'>
+          <p className='font-bold text-4xl my-3'>{formattedPrice(data?.current_price) || ''}</p>
+          <p className='my-auto'>{formattedPercentage(data?.price_change_percentage_24h) || ''}</p>
+        </div>)}
+        <Divider />
+        <div className='flex flex-col w-full border border-gray-700 rounded-md p-2'>
+          <p className='m-auto'>Market Cap</p>
+          <p className='m-auto'>{typeof data?.market_cap === 'number' && (
+            formatBillions(data?.market_cap)
+          )}</p>
+        </div>
+        <div className='flex flex-col w-full border border-gray-700 rounded-md p-2'>
+          <p className='m-auto'>Volume</p>
+          <p className='m-auto'>{typeof data?.total_volume === 'number' && (
+            formatBillions(data.total_volume)
+          )}</p>
+        </div>
+        <div className='flex flex-row gap-2'>
+          <div className='flex flex-col w-full border border-gray-700 rounded-md p-2'>
+            <p className='m-auto'>Total supply</p>
+            <p className='m-auto'>{typeof data?.circulating_supply === 'number' && (
+              formatSupply(data.circulating_supply)
+            )}</p>
+          </div>
+          <div className='flex flex-col w-full border border-gray-700 rounded-md p-2'>
+            <p className='m-auto'>Max supply</p>
+            {
+              data?.max_supply ? (
+                typeof data?.max_supply === 'number' && (<p className='m-auto'>{formatSupply(data.max_supply)}</p>)
+              ) : (<p className='m-auto'>{'???'}</p>)
+            }
+          </div>
+        </div>
+      </Card>
+    </div>
   )
 }
